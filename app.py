c116718fbe4deba3946ba6ff3101402b785763f6
Mustafa Zaki <mustafaosts@gmail.com>
2026-04-20 17:23:46 +0400

    Enhance QR code generator layout and functionality
    Refactor QR code generation layout with sub-columns and a button.

--- app.py
+++ app.py
@@ -7,23 +7,25 @@ st.markdown("<h1 style= 'text-align: center'>QR Code Generator</h1>", unsafe_all
 col1, col2, col3 = st.columns([1, 2, 1])
 
 with col2:
+
+ sub_col1, sub_col2 = st.columns([4, 1])
+
+ with sub_col1:
+  url = st.text_input("", placeholder= "https://www.example.com", label_visibility="collapsed")
+ with sub_col2:
+  generate_btn = st.button("Go")
+
  
- url = st.text_input("", placeholder= "https://www.example.com")
-
- if url: 
-
-  file_path = "qrcode.png"
+ if url:
+   qr = qrcode.QRCode()
+   qr.add_data(url)
 
 
-  qr = qrcode.QRCode()
-  qr.add_data(url)
+   img = qr.make_image()
 
 
-  img = qr.make_image()
+   buf = BytesIO()
+   img.save(buf, format="PNG")
+   byte_im = buf.getvalue()
 
-
-  buf = BytesIO()
-  img.save(buf, format="PNG")
-  byte_im = buf.getvalue()
-
-  st.image(byte_im, "Generated Successfully")
+   st.image(byte_im, "Generated Successfully")
